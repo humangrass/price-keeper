6ee0bd319eb84db273bb7f4056e18db9adcc543d
Alex Mikhankov <human.grass@gmail.com>
2025-05-21 01:21:09 +0300

    pair prices

--- internal/usecases/keeper/pair.go
+++ internal/usecases/keeper/pair.go
@@ -1,8 +1,10 @@
 package keeper
 
 import (
+	"context"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/humangrass/price-keeper/pgk/x/xhttp"
 )
@@ -22,6 +24,17 @@ func (uc *UseCase) handlePair(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type priceResponse struct {
+	Ticket  string                 `json:"ticket"`
+	Network map[string][]priceData `json:"network"`
+}
+
+type priceData struct {
+	Price     float64   `json:"price"`
+	Time      time.Time `json:"time"`
+	Timestamp int64     `json:"timestamp"`
+}
+
 func (uc *UseCase) getPair(w http.ResponseWriter, r *http.Request) error {
 	symbol := strings.TrimPrefix(r.URL.Path, "/api/pairs/")
 
@@ -29,8 +42,34 @@ func (uc *UseCase) getPair(w http.ResponseWriter, r *http.Request) error {
 	if len(parts) != 2 {
 		return xhttp.RespondWithError(w, http.StatusBadRequest, "Invalid symbol format")
 	}
-	// TODO:
-	return nil
+	prices, err := uc.priceRepository.FindBySymbols(context.Background(), parts[0], parts[1])
+	if err != nil {
+		uc.logger.Sugar().Error(err)
+		err = xhttp.RespondWithError(w, http.StatusInternalServerError, "Failed to retrieve tokens")
+		return err
+	}
+
+	response := priceResponse{
+		Ticket:  symbol,
+		Network: make(map[string][]priceData),
+	}
+
+	for _, price := range prices {
+		if price.Pair == nil {
+			continue
+		}
+
+		network := price.Pair.Numerator.Network
+		data := priceData{
+			Price:     price.Price,
+			Time:      price.TS,
+			Timestamp: price.TS.Unix(),
+		}
+
+		response.Network[network] = append(response.Network[network], data)
+	}
+
+	return xhttp.RespondWithJSON(w, http.StatusOK, response)
 }
 
 func (uc *UseCase) activatePair(w http.ResponseWriter, r *http.Request) error {
